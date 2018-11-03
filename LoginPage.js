8d44c4b3d96f3ce586c5618eee41c1a480ae4f58
Harry Chhabra <harrychhabra1998@gmail.com>
2018-11-03 10:10:23 +0530

    second commit

--- LoginPage.js
+++ LoginPage.js
@@ -3,6 +3,13 @@ function login()
 	var username = document.getElementById("username").value;
 	var pass = document.getElementById("password").value;
 	//Check for valid login page
-	alert("Homepage.html/"+username);
-	window.location.href="Homepage.html/"+username;
+	
+	if(username == "harry")
+	window.location.href="Homepage.html";
+	
+	if(username == "nitish")
+		window.location.href="InstituteAdmin.html";
+
+	if(username == "simran")
+		window.location.href="superAdminHomepage.html"
 }
